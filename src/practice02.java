2363a6c645acc01e239a84eb87c974652d6c62c4
0jun01 <dudwns85064843@gmail.com>
2025-03-17 16:54:05 +0900

    카운트 업
    정수 start_num와 end_num가 주어질 때, start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.

--- src/practice02.java
+++ src/practice02.java
@@ -128,6 +128,18 @@ public class practice02 {
         return sequence.stream().mapToInt(i -> i).toArray();
     }
 
+    public int[] solution10(int start_num, int end_num) {
+        int size = end_num - start_num + 1;
+        int[] answer = new int[size];
+
+        int index = 0;
+        for (int i = start_num; i <= end_num; i++) {
+            answer[index] = i;
+            index++;
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         int[] ad = solution5(5, 3);
         String my_String = "asd";
